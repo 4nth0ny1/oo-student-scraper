d07bbff5da5542af45fc5d5bd9b44a7719f9d5a6
4anth0ny1 <acatullo4@gmail.com>
2021-01-22 17:41:46 -0600

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,29 +3,46 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
+  attr_accessor :student
+  @@all = []
 
   def self.scrape_index_page(index_url)
-    html = File.read(index_url)
-    index_scrape = Nokogiri::HTML(html)
+    html = open(index_url)
 
-    hashes = {}
-    
-    index_scrape.css(div.rosters-card-container).each do |hash|
-      binding.pry
-      # student_card = hash.css(div.student-card).text
-      hashes[student_card.to_sym] = {
-        :name => hash.css(h4.student-name).text, 
-        :location => hash.css(p.student-location).text,
-        :profile_url => hash.css(div.student-card a).text
-        # attribute("src").value
+    page = Nokogiri::HTML(html)
+
+    students = []
+
+    page.css("div.student-card").each do |student|
+      students << {
+         :name => student.css("h4.student-name").text,
+         :location => student.css("p.student-location").text,
+         :profile_url => student.children[1].attributes["href"].value
       }
-      hashes
     end
+    students
   end
 
-  # def self.scrape_profile_page(profile_url)
-    
-  # end
+  def self.scrape_profile_page(profile_url)
+    page = Nokogiri::HTML(open(profile_url))
+
+    student_page = {}
+
+    social_links = page.css(".social-icon-container").css('a').collect {|e| e.attributes["href"].value}
+
+    social_links.detect do |e|
+
+      student_page[:twitter] = e if e.include?("twitter")
+      student_page[:linkedin] = e if e.include?("linkedin")
+      student_page[:github] = e if e.include?("github")
+
+    end
+
+    student_page[:blog] = social_links[3] if social_links[3] != nil
+    student_page[:profile_quote] = page.css(".profile-quote")[0].text
+    student_page[:bio] = page.css(".description-holder").css('p')[0].text
+    student_page
+  end
+
 
 end
-
